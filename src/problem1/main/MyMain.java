e9d9b3061ce576c6aeb31a1bf8950a3fd03e5aea
kshitij879 <kshitij.gupta_cs18@gla.ac.in>
2020-03-28 19:07:25 +0530

    TreeNode modified

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -4,8 +4,10 @@
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
-package problem1.main;
 // executable class
 
+package problem1.main;
+
 public class MyMain {
+
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,27 +11,33 @@ public class TreeNode {
     private TreeNode left;
     private TreeNode right;
 
-    public void setdata(int data){
+    public TreeNode(int data) {
         this.data = data;
+        this.left = null;
+        this.right = null;
     }
 
-    public int getdata(){
+    public int getData() {
         return data;
     }
 
-    public void setRight(TreeNode right){
-        this.right = right;
+    public void setData(int data) {
+        this.data = data;
     }
 
-    public void setLeft(TreeNode left){
-        this.left = left;
-    }
-
-    public TreeNode getLeft(){
+    public TreeNode getLeft() {
         return left;
     }
 
-    public TreeNode getRight(){
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
         return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
     }
 }
